12df914dcd0d1108e017c21bfc81b73a6bcc601e
Joshua Raphael <josh.n.raphael@gmail.com>
2025-07-29 21:43:55 -0700

    add headers and query parameters to http request

--- src/main/java/com/joshraphael/retroachievements4j/http/Request.java
+++ src/main/java/com/joshraphael/retroachievements4j/http/Request.java
@@ -1,12 +1,21 @@
 package com.joshraphael.retroachievements4j.http;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Request {
     private final String host;
     private final String path;
+    private final String method;
+    private final Map<String, String> queryParameters;
+    private final Map<String, String> headers;
 
     public Request(Builder builder) {
         this.host = builder.host;
         this.path = builder.path;
+        this.method = builder.method;
+        this.queryParameters = builder.queryParameters;
+        this.headers = builder.headers;
     }
 
     public String getHost() {
@@ -17,9 +26,24 @@ public class Request {
         return this.path;
     }
 
+    public String getMethod() {
+        return this.method;
+    }
+
+    public Map<String, String> getQueryParameters() {
+        return this.queryParameters;
+    }
+
+    public Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
     public static class Builder {
         private String host;
         private String path;
+        private String method;
+        private Map<String, String> queryParameters;
+        private Map<String, String> headers;
 
         public Builder host(String host) {
             this.host = host;
@@ -31,7 +55,44 @@ public class Request {
             return this;
         }
 
+        public Builder methodGET() {
+            this.method = "GET";
+            return this;
+        }
+
+        public Builder methodOPTIONS() {
+            this.method = "OPTIONS";
+            return this;
+        }
+
+        private Builder queryParameter(String key, String value) {
+            if(this.queryParameters == null) {
+                this.queryParameters = new HashMap<>();
+            }
+            this.queryParameters.put(key, value);
+            return this;
+        }
+
+        public Builder bearerToken(String token) {
+            return this.header("Authorization", String.format("Bearer %s", token));
+        }
+
+        public Builder userAgent(String userAgent) {
+            return this.header("User-Agent", userAgent);
+        }
+
+        private Builder header(String key, String value) {
+            if(this.headers == null) {
+                this.headers = new HashMap<>();
+            }
+            this.headers.put(key, value);
+            return this;
+        }
+
         public Request build() {
+            if(this.method == null || this.method.isBlank()) {
+                this.method = "GET";
+            }
             return new Request(this);
         }
     }
--- src/test/java/com/joshraphael/retroachievements4j/http/RequestTest.java
+++ src/test/java/com/joshraphael/retroachievements4j/http/RequestTest.java
@@ -6,8 +6,20 @@ import static org.junit.jupiter.api.Assertions.*;
 public class RequestTest {
     @Test
     void testBuilder() {
-        Request request = new Request.Builder().host("http://localhost").path("/api/v1/some_resource").build();
+        Request request = new Request.Builder()
+                .host("http://localhost")
+                .path("/api/v1/some_resource")
+                .methodGET()
+                .userAgent("retroachievements4j/v0.0.0")
+                .bearerToken("secret_bearer")
+                .build();
         assertEquals("http://localhost", request.getHost());
         assertEquals("/api/v1/some_resource", request.getPath());
+        assertEquals("GET", request.getMethod());
+        assertEquals(2, request.getHeaders().size());
+        assertTrue(request.getHeaders().containsKey("User-Agent"));
+        assertTrue(request.getHeaders().containsKey("Authorization"));
+        assertEquals("retroachievements4j/v0.0.0", request.getHeaders().get("User-Agent"));
+        assertEquals("Bearer secret_bearer", request.getHeaders().get("Authorization"));
     }
 }
