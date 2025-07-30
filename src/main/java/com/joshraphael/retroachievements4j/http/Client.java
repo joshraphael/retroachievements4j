9350566800361460374b3f02aea5cdc8ac79e059
Joshua Raphael <josh.n.raphael@gmail.com>
2025-07-29 22:08:11 -0700

    add basic client class

--- src/main/java/com/joshraphael/retroachievements4j/http/Client.java
+++ src/main/java/com/joshraphael/retroachievements4j/http/Client.java
@@ -1,4 +1,17 @@
 package com.joshraphael.retroachievements4j.http;
 
 public class Client {
+    private final String host;
+    private final String userAgent;
+
+    public Client(String host, String userAgent) {
+        this.host = host;
+        this.userAgent = userAgent;
+    }
+
+    public Request.Builder newRequestBuilder() {
+        return new Request.Builder()
+                .host(this.host)
+                .userAgent(this.userAgent);
+    }
 }
